880b1f205a79c9b5faec36f92f1257135f7a5ac1
mahidul-islam <mizihan84@gmail.com>
2022-05-15 15:48:06 +0600

    added main branch in the main url

--- lib/shared/dio/global_dio.dart
+++ lib/shared/dio/global_dio.dart
@@ -2,4 +2,4 @@ import 'package:dio/dio.dart';
 import 'package:sirah/shared/dio/dio_helper.dart';
 
 final Dio dio = DioHelper.getDio(
-    baseUrl: "https://flourishing-marigold-3d503b.netlify.app/");
+    baseUrl: "https://main--flourishing-marigold-3d503b.netlify.app/");
